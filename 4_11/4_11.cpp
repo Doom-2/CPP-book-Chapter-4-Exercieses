49919872162c3c54674e5d4a1ca742f6dfe6b5d1
doom-2 <phramov945@gmail.com>
2023-11-22 00:31:55 +0300

    Complete 4.11

---
+++ 4_11/4_11.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <std_lib_facilities.h>
+
+int main()
+{
+    vector<int> primes;
+	bool isPrime;
+	for (size_t i = 2; i <= 100; i++)
+	{
+		isPrime = true;
+		for (size_t j = 2; j <= sqrt(i); j++)
+		{
+			if (i % j == 0)
+			{
+				isPrime = false;
+				break;
+			}
+		}
+		if (isPrime) primes.push_back(i);
+	}
+	for (int val : primes) cout << val << endl;
+}
