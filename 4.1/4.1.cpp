a645fdcf24607d0ccfe76e208a5fb2650a2540ea
doom-2 <phramov945@gmail.com>
2023-11-28 23:59:36 +0300

    Complete 4.1

---
+++ 4.1/4.1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    vector<string> unwanted_words;
+    unwanted_words.push_back("Broccoli");
+    unwanted_words.push_back("Fuck");
+    unwanted_words.push_back("Damn");
+    vector<string> words;
+    cout << "Enter a few words. Separate input by pressing Enter or Space button.\n";
+    for (string temp; cin >> temp;)
+        words.push_back(temp);
+
+    for (string wrd : words)
+    {
+        for (string unwanted_word : unwanted_words)
+            if (wrd == unwanted_word) wrd = "BLEEP";
+        cout << wrd << endl;
+    }
+
+    return 0;
+}
